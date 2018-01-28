f7c0b47a12bbf14af49cd541ead868398d757d89
JScheng <coding520@163.com>
2018-01-28 21:49:55 +0800

    baseConverter

---
+++ test/baseConverter.test.js
@@ -0,0 +1,14 @@
+let assert = require('assert')
+let baseConverter = require('../src/Stack/baseConverter')
+
+describe('baseConverter', function () {
+    it('base2', function () {
+        assert.equal('10111', baseConverter(23, 2))
+    })
+    it('base8', function () {
+        assert.equal('351', baseConverter(233, 8))
+    })
+    it('base16', function () {
+        assert.equal('91D', baseConverter(2333, 16))
+    })
+})
