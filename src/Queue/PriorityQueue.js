490d0637a39d4baa224b7d98e7b3312155b25865
JScheng <coding520@163.com>
2018-02-16 01:23:37 +0800

    'update'

--- src/Queue/PriorityQueue.js
+++ src/Queue/PriorityQueue.js
@@ -1,5 +1,7 @@
 function PriorityQueue() {
+
     let items = []
+
     function QueueElement(element, priority) {
         this.element = element
         this.priority = priority
@@ -9,6 +11,35 @@ function PriorityQueue() {
         let queueElement = new QueueElement(element, priority)
 
         let added = false
-        
+        for (let i = 0; i < items.length; i++) {
+            if (queueElement.priority < items[i].priority) {
+                items.splice(i, 0, queueElement)
+                added = true
+                break
+            }
+        }
+
+        if (!added) {
+            items.push(queueElement)
+        }
+    }
+
+    this.dequeue = function () {
+        return items.shift()
+    }
+
+    this.front = function () {
+        return items[0]
+    }
+
+    this.isEmpty = function () {
+        return items.length === 0
+    }
+
+    this.print = function () {
+        for (let i = 0; i < items.length; i++) {
+            console.log(`${items[i].element} - ${items[i].priority}`);
+
+        }
     }
 }
