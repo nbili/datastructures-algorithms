8249192e1459c988a3c83409d07901d280926b24
JScheng <coding520@163.com>
2018-02-15 15:51:50 +0800

    'update'

---
+++ src/Queue/PriorityQueue.js
@@ -0,0 +1,14 @@
+function PriorityQueue() {
+    let items = []
+    function QueueElement(element, priority) {
+        this.element = element
+        this.priority = priority
+    }
+
+    this.enqueue = function (element, priority) {
+        let queueElement = new QueueElement(element, priority)
+
+        let added = false
+        
+    }
+}
