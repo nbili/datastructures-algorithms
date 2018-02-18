f99991778548613f021c10e0580610a54edaf01d
JScheng <coding520@163.com>
2018-02-18 23:19:25 +0800

    'update'

---
+++ test/PriorityQueue.test.js
@@ -0,0 +1,10 @@
+let assert = require('assert')
+let PriorityQueue = require('../src/Queue/PriorityQueue')
+
+let priorityQueue = new PriorityQueue()
+
+describe('priorityQueue', function () {
+    it('queue isEmpty should return true', function () {
+        assert.equal(true, priorityQueue.isEmpty())
+    })
+})
