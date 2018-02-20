4bc68640bd986235226db737df57ddf5e42f5999
JScheng <coding520@163.com>
2018-02-20 22:08:37 +0800

    update

--- src/Queue/PriorityQueue.js
+++ src/Queue/PriorityQueue.js
@@ -1,7 +1,18 @@
 let PriorityQueue = (function () {
 
     class QueueElement {
-        
+        constructor(element, priority) {
+            this.element = element
+            this.priority = priority
+        }
+    }
+
+    const items = new WeakMap()
+
+    class PriorityQueue {
+        constructor() {
+            items.set(this, [])
+        }
     }
 
 })()
