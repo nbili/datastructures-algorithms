77f4db572ed98c6cd1b3da9e4cbe2aeb793166e3
JScheng <coding520@163.com>
2018-02-21 16:51:47 +0800

    PriorityQueue

--- src/Queue/PriorityQueue.js
+++ src/Queue/PriorityQueue.js
@@ -13,6 +13,58 @@ let PriorityQueue = (function () {
         constructor() {
             items.set(this, [])
         }
-    }
 
+        enqueue(element, priority) {
+            const queueElement = new QueueElement(element, priority)
+
+            const q = items.get(this)
+
+            let added = false
+
+            for (let i = 0; i < q.length; i++) {
+                if (queueElement.priority < q[i].priority) {
+                    q.splice(i, 0, queueElement)
+                    added = true
+                    break
+                }
+            }
+            if (!added) {
+                q.push(queueElement)
+            }
+            items.set(this, q)
+        }
+
+        dequeue() {
+            let q = items.get(this)
+            let r = q.shift()
+            items.set(this, q)
+            return r
+        }
+
+        front() {
+            let q = items.get(this)
+            return q[0]
+        }
+
+        isEmpty() {
+            return items.get(this).length === 0
+        }
+
+        size() {
+            let q = items.get(this)
+            return q.length
+        }
+
+        clear() {
+            items.set(this, [])
+        }
+
+        print() {
+            let q = items.get(this)
+            for (let i = 0; i < q.length; i++) {
+                console.log(`${q[i].element} - ${q[i].priority}`)
+            }
+        }
+    }
+    return PriorityQueue
 })()
