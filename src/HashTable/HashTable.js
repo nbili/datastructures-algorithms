aa9ff7e4cdcfcb7799c6a81dc19f2ee9bc8ede02
JScheng <coding520@163.com>
2018-03-11 20:50:37 +0800

    update

--- src/HashTable/HashTable.js
+++ src/HashTable/HashTable.js
@@ -8,4 +8,30 @@ function HashTable() {
         }
         return hash % 37
     }
+
+    var djb2HashCode = function (key) {
+        var hash = 5381
+        for (let i = 0; i < key.length; i++) {
+            hash = hash * 33 + key.charCodeAt(i)
+        }
+        return hash % 1013
+    }
+
+    var hashCode = function (key) {
+        return loseloseHashCode(key)
+    }
+
+    this.put = function (key, value) {
+        var position = hashCode(key)
+        table[position] = value
+    }
+
+    this.get = function (key) {
+        return table[hashCode(key)]
+    }
+
+    this.remove = function (key) {
+        table[hashCode(key)] = undefined
+    }
+
 }
