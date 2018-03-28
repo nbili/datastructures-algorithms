e7f5189cece7a2a1d79ca13c476adb9db78b296b
jscheng <coding520@163.com>
2018-03-28 23:00:21 +0800

    insertionSort

--- src/Sort/SortingSearchingAlgorithms.js
+++ src/Sort/SortingSearchingAlgorithms.js
@@ -41,4 +41,24 @@ function ArrayList() {
             }
         }
     }
+
+    // insertionSort
+    this.insertionSort = function () {
+        var length = array.length,
+            j, temp
+        for (var i = 1; i < length; i++) {
+            j = i
+            temp = array[i]
+            while (j > 0 && array[j - 1] > temp) {
+                array[j] = array[j - 1]
+                j--
+            }
+            array[j] = temp
+        }
+    }
+
+    // mergeSort
+    this.mergeSort = function () {
+
+    }
 }
