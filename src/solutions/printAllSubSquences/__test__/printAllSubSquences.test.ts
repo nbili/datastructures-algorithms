175feaee7d89e6059ba853620bb3247e7432eab7
wucancheng <wucancheng@forchange.tech>
2021-11-13 12:11:10 +0800

    solutions

--- src/solutions/printAllSubSquences/__test__/printAllSubSquences.test.ts
+++ src/solutions/printAllSubSquences/__test__/printAllSubSquences.test.ts
@@ -1,10 +1,15 @@
-import { getSubs } from "../printAllSubSquences";
+import { getAll, getSubs } from "../printAllSubSquences";
 
-describe('getSubs', () => {
+describe("getSubs", () => {
+  it("get subs", () => {
+    const str = "abc";
 
-	it('get subs', () => {
-		const str = 'abc'
+    expect(getSubs(str).toString()).toBe("c,b,bc,a,ac,ab,abc");
+  });
 
-		expect(getSubs(str).toString()).toBe('c,b,bc,a,ac,ab,abc')
-	})
-})
+  it("get alls", () => {
+    const str = "abc";
+
+    expect(getAll(str).toString()).toBe("abc,acb,bac,bca,cba,cab");
+  });
+});
--- src/solutions/printAllSubSquences/printAllSubSquences.ts
+++ src/solutions/printAllSubSquences/printAllSubSquences.ts
@@ -21,4 +21,37 @@ function process(str: string, index: number, res: string[], path: string) {
   process(str, index + 1, res, (path += str[index]));
 }
 
+// 打印字符串的
+
+export function getAll(str: string) {
+  const res = [];
+
+  process2(str, 0, res);
+
+  return res;
+}
+
+function process2(str: string, index: number, res: string[]) {
+  if (index === str.length) {
+    res.push(str);
+  }
+
+  for (let j = index; j < str.length; j++) {
+    str = swap(str, index, j);
+
+    process2(str, index + 1, res);
+
+    str = swap(str, j, index);
+  }
+
+  function swap(str: string, a: number, b: number) {
+    const strAry = str.split("");
+    const temp = strAry[a];
+    strAry[a] = strAry[b];
+    strAry[b] = temp;
+
+    return strAry.join("");
+  }
+}
+
 export {};
